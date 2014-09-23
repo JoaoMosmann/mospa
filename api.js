202420dd2b109b6eaa0f5912083314ce75817cda
João Mosmann <jmosmann@doubleleft.com>
2014-09-23 14:07:40 -0300

    update public documentation

---
+++ api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "mospa.Event",
+        "mospa.EventHandler",
+        "mospa.MosPage"
+    ],
+    "modules": [
+        "mospa"
+    ],
+    "allModules": [
+        {
+            "displayName": "mospa",
+            "name": "mospa"
+        }
+    ]
+} };
+});
